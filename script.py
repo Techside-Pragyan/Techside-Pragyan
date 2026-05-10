07c5586e3b6b11c954761a527830ef6bcd8fb33a
Pragyan Paramita Moharana <moharanapragyanparamita@gmail.com>
2026-05-11 00:12:57 +0530

    feat: update tech stack visuals in README and refactor synchronization script

--- script.py
+++ script.py
@@ -1,9 +1,7 @@
-import codecs
-
 with open('README.md', 'r', encoding='utf-8') as f:
     readme = f.read()
 
-with codecs.open('algorithnicmind.md', 'r', encoding='utf-16le') as f:
+with open('algorithnicmind.md', 'r', encoding='utf-16le') as f:
     other = f.read()
 
 start_idx = other.find('Tech Stack')
@@ -11,19 +9,21 @@ if start_idx != -1:
     div_start = other.find('<div', start_idx)
     end_idx = other.find('GitHub Analytics', start_idx)
     
-    # We want everything from div_start up to the closing </div> before the ---
     tech_stack_content = other[div_start:end_idx].strip()
     
-    # Clean up any trailing '---' or '## ' that might have been caught
     last_div = tech_stack_content.rfind('</div>')
     if last_div != -1:
         tech_stack_content = tech_stack_content[:last_div + 6]
     
-    my_start = '## ??? Tech Stack & Tools'
-    my_end = '## ?? Currently Working On'
-
-    m_start_idx = readme.find(my_start)
-    m_end_idx = readme.find(my_end)
+    m_start_idx = readme.find('## ??? Tech Stack')
+    if m_start_idx == -1:
+        m_start_idx = readme.find('Tech Stack')
+        m_start_idx = readme.rfind('##', 0, m_start_idx)
+        
+    m_end_idx = readme.find('## ?? Currently Working On')
+    if m_end_idx == -1:
+        m_end_idx = readme.find('Currently Working On')
+        m_end_idx = readme.rfind('##', 0, m_end_idx)
 
     if m_start_idx != -1 and m_end_idx != -1:
         new_readme = readme[:m_start_idx] + '## ??? Tech Stack & Tools\n\n' + tech_stack_content + '\n\n---\n\n' + readme[m_end_idx:]
